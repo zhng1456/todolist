e56590e77b80e743f0ee418a871de434a9b23d32
zhng1456 <zhng1456@163.com>
2018-07-13 10:00:32 +0800

    add priority field

--- list/models.py
+++ list/models.py
@@ -9,3 +9,4 @@ class Thing(models.Model):
     id = models.IntegerField(primary_key=True)#主键
     task = models.CharField(max_length=300)#任务内容
     complete = models.BooleanField(default=False)#是否完成的标志位
+    priority = models.IntegerField(default=1)#新增优先级,数字越大级别越高，默认是1
