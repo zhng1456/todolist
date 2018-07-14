f0e3bb568c4662e52be0e7a93fd5ded1117afcd8
zhng1456 <zhng1456@163.com>
2018-07-14 18:51:47 +0800

    simplify urls

--- list/url.py
+++ list/url.py
@@ -8,5 +8,4 @@ urlpatterns = [
     url(r'^api/msg/$', views.MsgList.as_view()),#获取所有todo和新增todo
     url(r'^api/get-msg-order/$', views.get_msg_order),#用于排序
     url(r'^api/msg/(?P<pk>[0-9]+)', views.MsgDetail.as_view()),#修改和删除todo
-    #url(r'^api/edit-msg/(?P<pk>[0-9]+)', views.edit_msg),
 ]
